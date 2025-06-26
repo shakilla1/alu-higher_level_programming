b036aedfd51dfcfcfacaf1f0ec673d418423ef5e
shakilla1 <s.uwamahoro1@alustudent.com>
2025-06-26 04:36:01 -0700

    add a^b file

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-def fizzbuzz():
-    for number in range(1, 101):
-        if number % 3 == 0 and number % 5 == 0:
-            print("FizzBuzz ", end="")
-        elif number % 3 == 0:
-            print("Fizz ", end="")
-        elif number % 5 == 0:
-            print("Buzz ", end="")
-        else:
-            print("{} ".format(number), end="")
+def pow(a, b):
+    return(a ** b)
